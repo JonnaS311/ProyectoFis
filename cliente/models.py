9dee8e18ce43929ad51fb7e71c669e77f2245545
sotelo <jhonysotelo.1015@gmail.com>
2023-05-05 17:24:30 -0500

    interfaces y models

--- cliente/models.py
+++ cliente/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingrediente(models.Model):
+    precio_fijo = models.IntegerField()
+
+class Producto(models.Model):
+    precio_fijo = models.IntegerField()
+    #imagen = models.ImageField(upload_to='post/%Y/%m/%d')
+
+class Menu(models.Model):
+    precio_variable = models.IntegerField()
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    ingrediente = models.ForeignKey(Ingrediente,on_delete=models.CASCADE)
+    #imagen = models.ImageField(upload_to='post/%Y/%m/%d')
+
+class Restaurante(models.Model):
+    nombre = models.CharField(max_length=30)
+    telefono = models.PositiveIntegerField()
+    ingrediente = models.ForeignKey(Ingrediente,on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu,on_delete=models.CASCADE)
+
+class Sede(models.Model):
+    direccion_sede = models.CharField(max_length=30)
+    restaurante = models.ForeignKey(Restaurante,on_delete=models.CASCADE)
+
+class Pedido(models.Model):
+    fecha = models.DateField()
+    estado = models.CharField(max_length=30)
+    sede = models.ForeignKey(Sede, on_delete=models.CASCADE)
+
+class Cliente(models.Model):
+    name = models.CharField(max_length=30)
+    telefono = models.PositiveIntegerField()
+    direccion_cliente = models.CharField(max_length=30)
+    pedido = models.ForeignKey(Pedido,on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"nombre: {self.name}, telefono: {self.telefono}, direccion: {self.direccion_cliente}"
